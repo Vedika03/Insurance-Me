fbf53b819b372a0aae71cb304b7e51a3e7f502dc
Vedika Utekar <vedika@gmail.com>
2025-09-24 20:34:53 +0530

    Initial commit - InsureMe DevOps project

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO policy (policy_id, policy_holder, policy_type, premium_amount, start_date, end_date)
+VALUES ('P1001', 'Alice', 'Health', 1200.50, '2025-01-01', '2026-01-01');
+
+INSERT INTO policy (policy_id, policy_holder, policy_type, premium_amount, start_date, end_date)
+VALUES ('P1002', 'Bob', 'Car', 800.00, '2025-02-01', '2026-02-01');
+
+INSERT INTO policy (policy_id, policy_holder, policy_type, premium_amount, start_date, end_date)
+VALUES ('P1003', 'Charlie', 'Home', 1500.00, '2025-03-01', '2026-03-01');
