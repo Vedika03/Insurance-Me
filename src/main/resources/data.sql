cda2f7077658d50939142cea18cb8a67a24eea5f
Vedika Utekar <vedika@gmail.com>
2025-09-24 22:00:19 +0530

    quick fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,5 @@
-INSERT INTO policy (policy_id, policy_holder, policy_type, premium_amount, start_date, end_date)
-VALUES ('P1001', 'Alice', 'Health', 1200.50, '2025-01-01', '2026-01-01');
+INSERT INTO policy (policy_id, policy_type, policy_holder, premium_amount, start_date, end_date)
+VALUES (1, 'Health', 'Alice', 1200.50, '2025-01-01', '2026-01-01');
 
-INSERT INTO policy (policy_id, policy_holder, policy_type, premium_amount, start_date, end_date)
-VALUES ('P1002', 'Bob', 'Car', 800.00, '2025-02-01', '2026-02-01');
-
-INSERT INTO policy (policy_id, policy_holder, policy_type, premium_amount, start_date, end_date)
-VALUES ('P1003', 'Charlie', 'Home', 1500.00, '2025-03-01', '2026-03-01');
+INSERT INTO policy (policy_id, policy_type, policy_holder, premium_amount, start_date, end_date)
+VALUES (2, 'Car', 'Bob', 800.00, '2025-03-01', '2026-03-01');
